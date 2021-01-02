72216de15ce3806bfb07754e1c431c5861a6e3cc
waynntoh <waynnt98@gmail.com>
2021-01-02 15:42:57 +0800

    waynn

--- lib/objects/user.dart
+++ lib/objects/user.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class User {
   String email;
+  String name;
   String password;
   Image image;
 
-  User(String _email, String _password) {
+  User(String _email, String _name, String _password) {
     this.email = _email;
+    this.name = _name;
     this.password = _password;
   }
 
